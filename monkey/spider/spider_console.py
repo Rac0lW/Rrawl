efe1e4c26e72967307bc6de6212d116624850d51
RacolW <93457848+Rac0lW@users.noreply.github.com>
2023-03-17 19:40:22 +0800

    fix some bugs

---
+++ monkey/spider/spider_console.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+from importlib import import_module
+
+from monkey.config import Config
+from monkey.utils.log import logger
+
+def file_name(file_dir=os.path.join(Config.BASE_DIR, "spider/sources")):
+    """
+    Get spider class
+    :param file_dir:
+    :return:
+    """
+    all_files = []
+    for file in os.listdir(file_dir):
+        if file.endswith("_spider.py"):
+            all_files.append(file.replace(".py", ""))
+    return all_files
+
+
+def spider_console():
+    start = time.time()
+    all_files = file_name()
+    for spider in all_files:
+        spider_module = import_module("monkey.spider.sources.{}".format(spider))
+        spider_module.main()
+
+    logger.info(f"Time costs: {time.time() - start}")
+
+
+if __name__ == "__main__":
+    spider_console()
